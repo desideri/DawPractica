b07d59bbb9a28a03869fe9c79a80afe30d114153
mansrz <man.srz@gmail.com>
2016-01-16 12:17:34 -0500

    comment url not valid

--- web/urls.py
+++ web/urls.py
@@ -7,7 +7,6 @@ from web.views import *
 urlpatterns = [
     # Examples:
     # url(r'^$', 'daw.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', 'web.views.login'),
     url(r'^logout/', 'web.views.logout'),
